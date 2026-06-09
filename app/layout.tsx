1cbfd655323343ad87912e157bcf8c6c854aa70c
Jay Heo <yonseijaewon@gmail.com>
2026-06-09 20:54:42 +0900

    Add AdSense script and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6739714854685463"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
